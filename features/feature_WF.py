c1158dd191e683ad24db296bfe21178ab40553a0
Marjan Stojnev <35190706+stojnev@users.noreply.github.com>
2024-10-20 23:39:49 +0200

    Fixed an issue with stereo panning.

--- features/feature_WF.py
+++ features/feature_WF.py
@@ -11,8 +11,8 @@ def get_WF(audioStream, freqReference):
 
     FFTwindow = np.hanning(CHUNK)
 
-    # Declare all the required variables as "two channel" arrays.
-    FFTData = [0, 0], arrayFreq = [0, 0], arrayMagnitude = [0, 0], indexFFTL = [0, 0], freqDetected = [0, 0], freqDeviation = [0, 0], valueWowPercent = [0, 0], valueFlutter = [0, 0], valueFlutterPercent = [0, 0], valueWF = [0, 0]
+    # Declare the required variables as "stereo", two channel arrays.
+    FFTData, arrayFreq, arrayMagnitude, indexFFTL, freqDetected, freqDeviation, valueWowPercent, valueFlutter, valueFlutterPercent, valueWF = [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], 0, [0, 0], [0, 0], [0, 0], [0, 0]
 
     for channelX in range(CHANNELS):
         numDataM[channelX] = numDataM[channelX] * FFTwindow
@@ -21,7 +21,8 @@ def get_WF(audioStream, freqReference):
         arrayMagnitude[channelX] = np.abs(FFTData[channelX][:len(FFTData[channelX])//2])
         indexFFTL[channelX] = np.argmax(arrayMagnitude[channelX])
         freqDetected[channelX] = abs(arrayFreq[channelX][indexFFTL[channelX]])
-        valueWowPercent[channelX] = (abs(freqDeviation[channelX]) / freqReference) * 100
+        freqDeviation = freqDetected[channelX] - freqReference
+        valueWowPercent[channelX] = (abs(freqDeviation) / freqReference) * 100
         valueFlutter[channelX] = np.std(arrayMagnitude[channelX])
         valueFlutterPercent[channelX] = (valueFlutter[channelX] / np.max(arrayMagnitude[channelX])) * 100
         valueWF[channelX] = (valueWowPercent[channelX] + valueFlutterPercent[channelX]) / 2
