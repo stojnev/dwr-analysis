7abe72daaa9cd6b7eda3779484b41851adcbb98a
Marjan Stojnev <35190706+stojnev@users.noreply.github.com>
2024-10-22 16:20:28 +0200

    Cleaned up superfluous arrays in the W&F feature.

--- features/feature_WF.py
+++ features/feature_WF.py
@@ -16,19 +16,19 @@ def get_WF(audioStream, freqReference):
     FFTwindow = np.hanning(CHUNK)
 
     # Declare the required variables as "stereo", two channel arrays.
-    FFTData, arrayFreq, arrayMagnitude, indexFFTL, freqDetected, freqDeviation, valueWowPercent, valueFlutter, valueFlutterPercent, valueWF = [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], 0, [0, 0], [0, 0], [0, 0], [0, 0]
+    freqDetected, freqDeviation, valueWowPercent, valueFlutter, valueFlutterPercent, valueWF = [0, 0], 0, [0, 0], [0, 0], [0, 0], [0, 0]
 
     for channelX in range(CHANNELS):
         numDataM[channelX] = numDataM[channelX] * FFTwindow
-        FFTData[channelX] = np.fft.fft(numDataM[channelX])
-        arrayFreq[channelX] = np.fft.fftfreq(len(FFTData[channelX]), 1/RATE)
-        arrayMagnitude[channelX] = np.abs(FFTData[channelX][:len(FFTData[channelX])//2])
-        indexFFTL[channelX] = np.argmax(arrayMagnitude[channelX])
-        freqDetected[channelX] = abs(arrayFreq[channelX][indexFFTL[channelX]])
+        FFTData = np.fft.fft(numDataM[channelX])
+        arrayFreq = np.fft.fftfreq(len(FFTData), 1/RATE)
+        arrayMagnitude = np.abs(FFTData[:len(FFTData)//2])
+        indexFFTL = np.argmax(arrayMagnitude)
+        freqDetected[channelX] = abs(arrayFreq[indexFFTL])
         freqDeviation = freqDetected[channelX] - freqReference
         valueWowPercent[channelX] = (abs(freqDeviation) / freqReference) * 100
-        valueFlutter[channelX] = np.std(arrayMagnitude[channelX])
-        valueFlutterPercent[channelX] = (valueFlutter[channelX] / np.max(arrayMagnitude[channelX])) * 100
+        valueFlutter[channelX] = np.std(arrayMagnitude)
+        valueFlutterPercent[channelX] = (valueFlutter[channelX] / np.max(arrayMagnitude)) * 100
         valueWF[channelX] = (valueWowPercent[channelX] + valueFlutterPercent[channelX]) / 2
     
     return freqDetected, valueWowPercent, valueFlutterPercent, valueWF
